508db528536e74cb9a84fc2d38d7f3f9612e7ede
beery4010 <abhishek.tiwari.nitrr@gmail.com>
2026-08-09 12:44:47 +0530

    update: Added context parameter

--- loglens/exceptions.py
+++ loglens/exceptions.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import sys
+from typing import Any
 
 
 class ApplicationException(Exception):
@@ -19,30 +20,36 @@ class ApplicationException(Exception):
     def __init__(
         self,
         error_message: str,
-        sys_error_details: Exception | None = None,
+        sys_error_details: BaseException | None = None,
+        **context: Any
     ) -> None:
         _, _, tb = sys.exc_info()
         if tb is not None:
-            self.lineno = tb.tb_lineno
-            self.filename = tb.tb_frame.f_code.co_filename
+            self.lineno: Any = tb.tb_lineno
+            self.filename: str = tb.tb_frame.f_code.co_filename
         else:
             self.lineno = "NA"
             self.filename = "NA"
 
         self.error_message = error_message
         self.sys_error_details = sys_error_details
+        self.context = context
         super().__init__(self.error_message)
 
     def __str__(self) -> str:
         """
         Render the exception in a single log-friendly line.
 
-        Format: `filename:lineno | message`
+        Format: `filename:lineno | message | context`
 
         Returns:
             str: Compact one-line representation.
         """
-        return f"{self.filename}:{self.lineno} | {self.args[0]}"
+        base =  f"{self.filename}:{self.lineno} | {self.args[0]}"
+        if self.context:
+            rendered = " ".join(f"{k}={v!r}" for k, v in self.context.items())
+            return f"{base} | {rendered}"
+        return base
 
     def __repr__(self) -> str:
         """
@@ -53,5 +60,6 @@ class ApplicationException(Exception):
         """
         return (
             f"ApplicationException(filename={self.filename!r}, "
-            f"lineno={self.lineno!r}, message={self.args[0]!r})"
+            f"lineno={self.lineno!r}, message={self.args[0]!r}), "
+            f"contect={self.context!r}"
         )
